b2b1ef81309366799c1cfd839de10bf685da8b72
Kishore Ramanan <kishorramanan@gmail.com>
2016-05-01 09:14:10 +0530

    New Docker Files

---
+++ jenkins_master/custom scripts/00 set_authorization.groovy
@@ -0,0 +1,23 @@
+import jenkins.model.Jenkins
+import jenkins.model.*
+import hudson.model.User
+import hudson.security.*
+
+String[] arg = ['1236', 'admin', 'Y'] as String[]
+def instance = Jenkins.getInstance()
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+def allUsers = User.getAll()
+for (u in allUsers) {
+ if (u.getId()== arg[0] ) {
+	println "${arg[0]} all ready exists in Jenkins"
+	}else {
+	 hudsonRealm.createAccount(arg[0], arg[1])
+	 if (arg[2] =="Y"){
+		 def strategy = new GlobalMatrixAuthorizationStrategy()
+		 strategy.add(Jenkins.ADMINISTER, arg[0])
+		 instance.setAuthorizationStrategy(strategy)
+	 }
+	 instance.setSecurityRealm(hudsonRealm)
+	 instance.save()
+ }
+}
