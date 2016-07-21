c458cd7c7f123101bcbfeededebaeb75cf3acd49
kishor <kishor>
2016-07-21 10:03:40 +0000

    modified:   jenkins_master/Dockerfile modified:   jenkins_master/jenkins.sh

--- jenkins_master/jenkins.sh
+++ jenkins_master/jenkins.sh
@@ -3,23 +3,23 @@
 set -e
 
 # Copy files from /usr/share/jenkins/ref into /var/jenkins_home
-# So the initial JENKINS-HOME is set with expected content. 
-# Don't override, as this is just a reference setup, and use from UI 
+# So the initial JENKINS-HOME is set with expected content.
+# Don't override, as this is just a reference setup, and use from UI
 # can then change this, upgrade plugins, etc.
 copy_reference_file() {
-	f=${1%/} 
+	f=${1%/}
 	echo "$f" >> $COPY_REFERENCE_FILE_LOG
     rel=${f:23}
     dir=$(dirname ${f})
     echo " $f -> $rel" >> $COPY_REFERENCE_FILE_LOG
-	if [[ ! -e /var/jenkins_home/${rel} ]] 
+	if [[ ! -e /var/jenkins_home/${rel} ]]
 	then
 		echo "copy $rel to JENKINS_HOME" >> $COPY_REFERENCE_FILE_LOG
 		mkdir -p /var/jenkins_home/${dir:23}
 		cp -r /usr/share/jenkins/ref/${rel} /var/jenkins_home/${rel};
 		# pin plugins on initial copy
 		[[ ${rel} == plugins/*.jpi ]] && touch /var/jenkins_home/${rel}.pinned
-	fi; 
+	fi;
 }
 export -f copy_reference_file
 echo "--- Copying files at $(date)" >> $COPY_REFERENCE_FILE_LOG
@@ -28,8 +28,9 @@ find /usr/share/jenkins/ref/ -type f -exec bash -c "copy_reference_file '{}'" \;
 # if `docker run` first argument start with `--` the user is passing jenkins launcher arguments
 if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
    exec java $JAVA_OPTS -jar /opt/jenkins.war $JENKINS_OPTS "$@"
+	 sleep 10
+	 exec jenkins-jobs --conf /opt/jjb/jenkins_jobs.ini update /opt/jjb/*.yaml
 fi
 
 # As argument is not jenkins, assume user want to run his own process, for sample a `bash` shell to explore this image
 exec "$@"
-
