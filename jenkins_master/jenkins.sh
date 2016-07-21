a4663fa5fc6c60e1637a2f1a2ac7937fee760374
kishor <kishor>
2016-07-21 13:10:52 +0000

    modified:   jenkins_master/jenkins.sh

--- jenkins_master/jenkins.sh
+++ jenkins_master/jenkins.sh
@@ -28,8 +28,8 @@ find /usr/share/jenkins/ref/ -type f -exec bash -c "copy_reference_file '{}'" \;
 # if `docker run` first argument start with `--` the user is passing jenkins launcher arguments
 if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
    exec java $JAVA_OPTS -jar /opt/jenkins.war $JENKINS_OPTS "$@"
-	 sleep 10
-	 exec jenkins-jobs --conf /opt/jjb/jenkins_jobs.ini update /opt/jjb/*.yaml
+   cd /opt/jjb/
+	 exec jenkins-jobs --conf jenkins_jobs.ini update job.yaml
 fi
 
 # As argument is not jenkins, assume user want to run his own process, for sample a `bash` shell to explore this image
